538f3de34f1d359f97dbc2f6ddf77fec784f5742
GitzJoey <gitzjoey@gmail.com>
2016-09-14 00:12:37 +0700

    new ide-helper

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2016-09-11.
+    * Generated on 2016-09-13.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
