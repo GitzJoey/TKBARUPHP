b54cc82f8a51d90e7bab322e3d1904b8db52e60b
Peteng Dedet <pueteng.dedet@gmail.com>
2016-09-19 22:54:40 +0700

    Update Customer.php

--- app/Customer.php
+++ app/Customer.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: GitzJoey
- * Date: 9/7/2016
- * Time: 12:16 AM
- */
+// PetengDedet
 
 namespace App;
 
@@ -17,6 +12,18 @@ use \Illuminate\Database\Eloquent\Model;
  */
 class Customer extends Model
 {
-    protected $table = 'customer';
+    protected $table = 'customers';
+	protected $fillable = [
+        'name', 'address', 'city', 'phone', 'remarks', 'tax_id', 'payment_due_day'
+    ];
 
+    public function profile()
+    {
+        return $this->hasMany('\App\Profile', 'phone_number', 'phone');
+    }
+
+    public function bank()
+    {
+        // return $this->hasMany('\App\Bank');
+    }
 }
