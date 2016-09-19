53220c01479b402e198ba3ba84df2bdf8a1c5681
Peteng Dedet <pueteng.dedet@gmail.com>
2016-09-19 22:58:23 +0700

    Create customer.php

---
+++ resources/lang/id/customer.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+	'icon' 								=> 'home',
+	'name' 								=> 'Nama',
+	'address' 							=> 'Alamat',
+	'city' 								=> 'Kota',
+	'phone' 							=> 'No. Telepon',
+	'remarks' 							=> 'Remarks',
+	'tax_id' 							=> 'Tax ID',
+	'payment_due_date' 					=> 'Payment Due Date',
+
+    'index.title' 						=> 'Manajemen Pelanggan',
+    'index.page_title' 					=> 'Pelanggan'
+    'index.table.header.name' 			=> 'Nama',
+	'index.table.header.address' 		=> 'Alamat',
+	'index.table.header.city' 			=> 'Kota',
+	'index.table.header.phone' 			=> 'Telepon',
+	'index.table.header.remarks' 		=> 'Remarks',
+	'index.table.header.taxt_id' 		=> 'Tax ID',
+	'index.table.header.action' 		=> 'Action',
+	'index.button.create' 				=> 'Tambah Pelanggan',
+
+	'create.title' 						=> 'Buat Pelanggan Baru'
+
+];
