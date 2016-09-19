b199cd58268da5fdb2f7702cdec44657a0a12718
Peteng Dedet <pueteng.dedet@gmail.com>
2016-09-19 22:56:50 +0700

    Create 2016_09_18_130835_create_customer_table.php

---
+++ database/migrations/2016_09_18_130835_create_customer_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->text('address');
+            $table->string('city', 100);
+            $table->string('phone', 50);
+            $table->text('remarks');
+            $table->string('tax_id', 100);
+            $table->nullableTimestamps('payment_due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('customers');
+    }
+}
