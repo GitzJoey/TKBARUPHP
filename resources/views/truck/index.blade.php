f61efeb76886b6510b73d3470e533fab2d17e595
gitzjoey <gitzjoey@live.com>
2016-09-21 11:03:31 +0700

    clean up truck

--- resources/views/truck/index.blade.php
+++ resources/views/truck/index.blade.php
@@ -20,7 +20,7 @@
 
     <div class="box box-info">
         <div class="box-header with-border">
-            <h3 class="box-title">@lang('truck.index.table.header.title')</h3>
+            <h3 class="box-title">@lang('truck.index.header.title')</h3>
         </div>
         <div class="box-body">
             <table class="table table-bordered">
@@ -43,14 +43,10 @@
                         <td>@lang('lookup.' . $truck->status)</td>
                         <td>{{ $truck->remarks }}</td>
                         <td class="text-center" width="20%">
-                            <a class="btn btn-xs btn-info" href="{{ route('db.master.truck.show', $truck->id) }}"><span
-                                        class="fa fa-info fa-fw"></span></a>
-                            <a class="btn btn-xs btn-primary"
-                               href="{{ route('db.master.truck.edit', $truck->id) }}"><span
-                                        class="fa fa-pencil fa-fw"></span></a>
+                            <a class="btn btn-xs btn-info" href="{{ route('db.master.truck.show', $truck->id) }}"><span class="fa fa-info fa-fw"></span></a>
+                            <a class="btn btn-xs btn-primary" href="{{ route('db.master.truck.edit', $truck->id) }}"><span class="fa fa-pencil fa-fw"></span></a>
                             {!! Form::open(['method' => 'DELETE', 'route' => ['db.master.truck.delete', $truck->id], 'style'=>'display:inline'])  !!}
-                            <button type="submit" class="btn btn-xs btn-danger"><span class="fa fa-close fa-fw"></span>
-                            </button>
+                                <button type="submit" class="btn btn-xs btn-danger"><span class="fa fa-close fa-fw"></span></button>
                             {!! Form::close() !!}
                         </td>
                     </tr>
@@ -59,8 +55,7 @@
             </table>
         </div>
         <div class="box-footer clearfix">
-            <a class="btn btn-success" href="{{ route('db.master.truck.create') }}"><span
-                        class="fa fa-plus fa-fw"></span>&nbsp;@lang('buttons.create_new_button')</a>
+            <a class="btn btn-success" href="{{ route('db.master.truck.create') }}"><span class="fa fa-plus fa-fw"></span>&nbsp;@lang('buttons.create_new_button')</a>
             {!! $trucklist->render() !!}
         </div>
     </div>
--- resources/views/truck/show.blade.php
+++ resources/views/truck/show.blade.php
@@ -23,13 +23,13 @@
 @section('content')
     <div class="box box-info">
         <div class="box-header with-border">
-            <h3 class="box-title">{{ $truck->plate_number }}</h3>
+            <h3 class="box-title">@lang('truck.show.header.title') : {{ $truck->plate_number }}</h3>
         </div>
         <div class="box-body">
             <form class="form-horizontal">
                 <div class="box-body">
                     <div class="form-group">
-                        <label for="inputPlateNumber" class="col-sm-2 control-label">@lang('truck.plate_number')</label>
+                        <label for="inputPlateNumber" class="col-sm-2 control-label">@lang('truck.field.plate_number')</label>
                         <div class="col-sm-10">
                             <label id="plateNumber" class="control-label">
                                 <span class="control-label-normal">{{ $truck->plate_number }}</span>
@@ -37,7 +37,7 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="inputInspectionDate" class="col-sm-2 control-label">@lang('truck.inspection_date')</label>
+                        <label for="inputInspectionDate" class="col-sm-2 control-label">@lang('truck.field.inspection_date')</label>
                         <div class="col-sm-10">
                             <label id="inspectionDate" class="control-label">
                                 <span class="control-label-normal">{{ $truck->inspection_date }}</span>
@@ -45,7 +45,7 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="inputDriver" class="col-sm-2 control-label">@lang('truck.driver')</label>
+                        <label for="inputDriver" class="col-sm-2 control-label">@lang('truck.field.driver')</label>
                         <div class="col-sm-10">
                             <label id="driver" class="control-label control-label-normal">
                                 <span class="control-label-normal">{{ $truck->driver }}</span>
@@ -53,7 +53,7 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="inputStatus" class="col-sm-2 control-label">@lang('truck.status')</label>
+                        <label for="inputStatus" class="col-sm-2 control-label">@lang('truck.field.status')</label>
                         <div class="col-sm-10">
                             <label id="status" class="control-label control-label-normal">
                                 <span class="control-label-normal">@lang('lookup.' . $truck->status)</span>
@@ -61,21 +61,21 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="inputRemarks" class="col-sm-2 control-label">@lang('truck.remarks')</label>
+                        <label for="inputRemarks" class="col-sm-2 control-label">@lang('truck.field.remarks')</label>
                         <div class="col-sm-10">
                             <label id="remarks" class="control-label control-label-normal">
                                 <span class="control-label-normal">{{ $truck->remarks }}</span>
                             </label>
                         </div>
                     </div>
+                </div>
+                <div class="box-footer">
                     <div class="form-group">
                         <label for="inputButton" class="col-sm-2 control-label"></label>
                         <div class="col-sm-10">
                             <a href="{{ route('db.master.truck') }}" class="btn btn-default">@lang('buttons.show.cancel')</a>
                         </div>
                     </div>
-                </div>
-                <div class="box-footer">
                 </div>
             </form>
         </div>
