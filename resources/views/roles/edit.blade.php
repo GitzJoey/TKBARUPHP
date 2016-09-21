080ccbf0f2b0566cf533a6dfdf64af33a9eda20b
gitzjoey <gitzjoey@live.com>
2016-09-21 14:50:20 +0700

    recommit  role

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -25,31 +25,31 @@
 
     <div class="box box-info">
         <div class="box-header with-border">
-            <h3 class="box-title">@lang('role.edit.title')</h3>
+            <h3 class="box-title">@lang('role.edit.header.title')</h3>
         </div>
 
         {!! Form::model($role, ['method' => 'PATCH','route' => ['db.admin.role.edit', $role->hId()], 'class' => 'form-horizontal']) !!}
             <div class="box-body">
                 <div class="form-group">
-                    <label for="inputName" class="col-sm-2 control-label">@lang('role.name')</label>
+                    <label for="inputName" class="col-sm-2 control-label">@lang('role.field.name')</label>
                     <div class="col-sm-10">
-                        <input id="inputName" name="name" type="text" class="form-control" placeholder="@lang('role.name')">
+                        <input id="inputName" name="name" type="text" class="form-control" placeholder="@lang('role.field.name')">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="inputDisplayName" class="col-sm-2 control-label">@lang('role.display_name')</label>
+                    <label for="inputDisplayName" class="col-sm-2 control-label">@lang('role.field.display_name')</label>
                     <div class="col-sm-10">
-                        <input id="inputDisplayName" name="display_name" type="text" class="form-control" placeholder="@lang('role.display_name')">
+                        <input id="inputDisplayName" name="display_name" type="text" class="form-control" placeholder="@lang('role.field.display_name')">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="inputDescription" class="col-sm-2 control-label">@lang('role.description')</label>
+                    <label for="inputDescription" class="col-sm-2 control-label">@lang('role.field.description')</label>
                     <div class="col-sm-10">
-                        <input id="inputDescription" name="display_name" type="text" class="form-control" placeholder="@lang('role.description')">
+                        <input id="inputDescription" name="display_name" type="text" class="form-control" placeholder="@lang('role.field.description')">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="inputPermission" class="col-sm-2 control-label">@lang('role.permission')</label>
+                    <label for="inputPermission" class="col-sm-2 control-label">@lang('role.field.permission')</label>
                     <div class="col-sm-10">
                         <select multiple class="form-control" data-height="20">
                             @foreach($permission as $key => $p)
@@ -58,11 +58,13 @@
                         </select>
                     </div>
                 </div>
+            </div>
+            <div class="box-footer">
                 <div class="form-group">
                     <label for="inputButton" class="col-sm-2 control-label"></label>
                     <div class="col-sm-10">
-                        <a href="{{ route('db.admin.roles') }}" class="btn btn-default">Cancel</a>
-                        <button class="btn btn-default" type="submit">Submit</button>
+                        <a href="{{ route('db.admin.roles') }}" class="btn btn-default">@lang('buttons.cancel_button')</a>
+                        <button class="btn btn-default" type="submit">@lang('buttons.submit_button')</button>
                     </div>
                 </div>
             </div>
