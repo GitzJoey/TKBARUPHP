7b41834bca0f3a6364f5dcf6f39c4a295bbec1b6
GitzJoey <gitzjoey@gmail.com>
2016-10-04 01:13:05 +0700

    prepare table for softdelete and action by userid

--- database/migrations/2016_09_23_092551_create_supplier_profile_table.php
+++ database/migrations/2016_09_23_092551_create_supplier_profile_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSupplierPicTable extends Migration
+class CreateSupplierProfileTable extends Migration
 {
     /**
      * Run the migrations.
