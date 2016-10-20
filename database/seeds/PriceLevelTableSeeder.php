6208b8239cac5d3a68512aaa06c1a42b6467155b
gitzjoey <gitzjoey@live.com>
2016-10-20 10:59:25 +0700

    add price level table seedr

---
+++ database/seeds/PriceLevelTableSeeder.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sugito
+ * Date: 10/20/2016
+ * Time: 10:46 AM
+ */
+
+use App\PriceLevel;
+
+use Illuminate\Database\Seeder;
+
+class PriceLevelTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $pp = [
+            [
+                'store_id' => 1,
+                'type' => 'PRICELEVELTYPE.inc',
+                'weight' => 0,
+                'name' => 'RETAIL',
+                'description' => 'Retail / Eceran',
+                'increment_value' => 200,
+                'percentage_value' => 0,
+                'status' => 'STATUS.Active',
+            ],
+            [
+                'store_id' => 1,
+                'type' => 'PRICELEVELTYPE.inc',
+                'weight' => 1,
+                'name' => 'WHOLESALE 1',
+                'description' => 'Wholesale / Grosir Type 1',
+                'increment_value' => 100,
+                'percentage_value' => 0,
+                'status' => 'STATUS.Active',
+            ],
+            [
+                'store_id' => 1,
+                'type' => 'PRICELEVELTYPE.inc',
+                'weight' => 2,
+                'name' => 'WHOLESALE 2',
+                'description' => 'Wholesale / Grosir Type 2',
+                'increment_value' => 50,
+                'percentage_value' => 0,
+                'status' => 'STATUS.Active',
+            ],
+        ];
+
+        foreach ($pp as $key => $value) {
+            PriceLevel::create($value);
+        }
+    }
+}
