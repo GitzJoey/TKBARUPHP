3cdf69d1773c2123edf84da322bda90be213a5ef
GitzJoey <gitzjoey@gmail.com>
2016-10-29 06:42:51 +0700

    remove adminlte folder in public

--- gulpfile.js
+++ gulpfile.js
@@ -48,9 +48,9 @@ elixir(function (mix) {
         './node_modules/jquery-gotop/src/jquery.gotop.min.js',
         './resources/assets/js/adminlte.custom.js'
     ], 'public/adminlte/js/app.js')
-        .copy('node_modules/bootstrap/fonts', 'public/adminlte/fonts')
-        .copy('node_modules/font-awesome/fonts', 'public/adminlte/fonts')
-        .copy('node_modules/ionicons/dist/fonts', 'public/adminlte/fonts')
-        .copy('node_modules/icheck/skins/square/blue.png', 'public/adminlte/css')
-        .copy('node_modules/icheck/skins/square/blue@2x.png', 'public/adminlte/css');
+    .copy('node_modules/bootstrap/fonts', 'public/adminlte/fonts')
+    .copy('node_modules/font-awesome/fonts', 'public/adminlte/fonts')
+    .copy('node_modules/ionicons/dist/fonts', 'public/adminlte/fonts')
+    .copy('node_modules/icheck/skins/square/blue.png', 'public/adminlte/css')
+    .copy('node_modules/icheck/skins/square/blue@2x.png', 'public/adminlte/css');
 });
