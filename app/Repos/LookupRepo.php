118dbff4fd8a7df38ce3e1a2ce92347b1ad6c135
gitzjoey <gitzjoey@live.com>
2016-10-31 09:05:36 +0700

    formatting

--- app/Repos/LookupRepo.php
+++ app/Repos/LookupRepo.php
@@ -3,15 +3,16 @@
 namespace App\Repos;
 
 use Illuminate\Support\Facades\Cache;
+
 use App\Model\Lookup;
 
 class LookupRepo
 {
     public function findByCategory($category)
     {
-    	$lookups = Cache::remember('lookups_by_category_'.$category, 10, function() use($category) {
-		    return Lookup::where('category', '=', $category)->get()->pluck('description', 'code');
-		});
-    	return $lookups;
+        $lookups = Cache::remember('lookups_by_category_' . $category, 10, function () use ($category) {
+            return Lookup::where('category', '=', $category)->get()->pluck('description', 'code');
+        });
+        return $lookups;
     }
 }
