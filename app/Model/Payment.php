638b3524b427a94513bfbbc487ad0037adb60d39
Furyslam <miftah.9g@gmail.com>
2016-11-08 00:10:06 +0700

    Payment model.

---
+++ app/Model/Payment.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+
+class Payment extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $hidden = [
+        'payment_detail_type', 'payable_type'
+    ];
+
+    public function payment_detail()
+    {
+        return $this->morphTo();
+    }
+
+    public function payable()
+    {
+        return $this->morphTo();
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $user = Auth::user();
+            if ($user) {
+                $model->created_by = $user->id;
+                $model->updated_by = $user->id;
+            }
+        });
+
+        static::updating(function ($model) {
+            $user = Auth::user();
+            if ($user) {
+                $model->updated_by = $user->id;
+            }
+        });
+
+        static::deleting(function ($model) {
+            $user = Auth::user();
+            if ($user) {
+                $model->deleted_by = $user->id;
+                $model->save();
+            }
+        });
+    }
+}
