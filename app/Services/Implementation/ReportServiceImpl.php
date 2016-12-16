16763cf44cb2d03de28c41900fe62b54f887cfda
GitzJoey <gitzjoey@gmail.com>
2016-12-16 19:38:49 +0700

    remove file if older than 1 day

--- app/Services/Implementation/ReportServiceImpl.php
+++ app/Services/Implementation/ReportServiceImpl.php
@@ -8,13 +8,21 @@
 
 namespace App\Services\Implementation;
 
+use File;
 use Storage;
+use Carbon\Carbon;
 use App\Services\ReportService;
 
 class ReportServiceImpl implements ReportService
 {
     public function doReportHousekeeping()
     {
-        dd(Storage::disk('report_storage')->allFiles());
+        foreach (Storage::disk('report_storage')->files() as $filename) {
+            $processFile = storage_path("app/public/reports/").$filename;
+
+            if (Carbon::createFromFormat('U', File::lastModified($processFile))->diffInDays(Carbon::now()) > 0) {
+                File::delete($processFile);
+            }
+        }
     }
 };
