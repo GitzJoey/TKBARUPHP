e898dc65e55cd1b8473ad00196ad732e1aff316b
GitzJoey <gitzjoey@gmail.com>
2017-01-09 01:05:53 +0700

    start including vue

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ elixir(function (mix) {
         './node_modules/jquery/dist/jquery.min.js',
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
         './node_modules/angular/angular.min.js',
+        './node_modules/vue/dist/vue.min.js',
         './node_modules/angular-sanitize/angular-sanitize.min.js',
         './node_modules/moment/min/moment.min.js',
         './node_modules/moment-duration-format/lib/moment-duration-format.js',
