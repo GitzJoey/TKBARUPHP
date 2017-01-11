a773ce868b22174c29b5b0b0be59544618d709ea
gitzjoey <gitzjoey@live.com>
2017-01-11 15:46:30 +0700

    replace fcsa-number using autoNumeric with sample implementation

--- resources/views/expense_template/create.blade.php
+++ resources/views/expense_template/create.blade.php
@@ -30,80 +30,74 @@
         </div>
         <form class="form-horizontal" action="{{ route('db.master.expense_template.create') }}" method="post" data-parsley-validate="parsley">
             {{ csrf_field() }}
-            <div ng-app="expenseTemplateModule" ng-controller="expenseTemplateController">
-                <div class="box-body">
-                    <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
-                        <label for="inputName" class="col-sm-2 control-label">@lang('expense_template.field.name')</label>
-                        <div class="col-sm-8">
-                            <input id="inputName" name="name" type="text" class="form-control" placeholder="@lang('expense_template.field.name')"
-                                   data-parsley-required="true">
-                            <span class="help-block">{{ $errors->has('name') ? $errors->first('name') : '' }}</span>
-                        </div>
+            <div class="box-body">
+                <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+                    <label for="inputName" class="col-sm-2 control-label">@lang('expense_template.field.name')</label>
+                    <div class="col-sm-8">
+                        <input id="inputName" name="name" type="text" class="form-control" placeholder="@lang('expense_template.field.name')"
+                               data-parsley-required="true">
+                        <span class="help-block">{{ $errors->has('name') ? $errors->first('name') : '' }}</span>
                     </div>
-                    <div class="form-group {{ $errors->has('type') ? 'has-error' : '' }}">
-                        <label for="inputType" class="col-sm-2 control-label">@lang('expense_template.field.type')</label>
-                        <div class="col-sm-8">
-                            {{ Form::select('type', $expenseTypes, null, array('class' => 'form-control', 'placeholder' => Lang::get('labels.PLEASE_SELECT'), 'data-parsley-required' => 'true')) }}
-                            <span class="help-block">{{ $errors->has('type') ? $errors->first('type') : '' }}</span>
-                        </div>
+                </div>
+                <div class="form-group {{ $errors->has('type') ? 'has-error' : '' }}">
+                    <label for="inputType" class="col-sm-2 control-label">@lang('expense_template.field.type')</label>
+                    <div class="col-sm-8">
+                        {{ Form::select('type', $expenseTypes, null, array('class' => 'form-control', 'placeholder' => Lang::get('labels.PLEASE_SELECT'), 'data-parsley-required' => 'true')) }}
+                        <span class="help-block">{{ $errors->has('type') ? $errors->first('type') : '' }}</span>
                     </div>
-                    <div class="form-group {{ $errors->has('amount') ? 'has-error' : '' }}">
-                        <label for="inputAmount" class="col-sm-2 control-label">@lang('expense_template.field.amount')</label>
-                        <div class="col-sm-8">
-                            <input id="inputAmount"
-                                   name="amount"
-                                   type="text"
-                                   class="form-control"
-                                   placeholder="@lang('expense_template.field.amount')"
-                                   data-parsley-required="true"
-                                   data-parsley-pattern="/^\d+(,\d+)*$/"
-                                   ng-model="amount" fcsa-number>
-                            <span class="help-block">{{ $errors->has('amount') ? $errors->first('amount') : '' }}</span>
-                        </div>
+                </div>
+                <div class="form-group {{ $errors->has('amount') ? 'has-error' : '' }}">
+                    <label for="inputAmount" class="col-sm-2 control-label">@lang('expense_template.field.amount')</label>
+                    <div class="col-sm-8">
+                        <input id="inputAmount"
+                               name="amount"
+                               type="text"
+                               class="form-control"
+                               placeholder="@lang('expense_template.field.amount')"
+                               data-parsley-required="true"
+                               data-parsley-pattern="/^\d+(,\d+)*$/"
+                               ng-model="amount" autonumeric data-a-sep="," data-a-dec=".">
+                        <span class="help-block">{{ $errors->has('amount') ? $errors->first('amount') : '' }}</span>
                     </div>
-                    <div class="form-group {{ $errors->has('remarks') ? 'has-error' : '' }}">
-                        <label for="inputRemarks" class="col-sm-2 control-label">@lang('expense_template.field.remarks')</label>
-                        <div class="col-sm-8">
-                            <input id="inputRemarks" name="remarks" type="text" class="form-control" placeholder="@lang('expense_template.field.remarks')"
-                                   data-parsley-required="true">
-                            <span class="help-block">{{ $errors->has('remarks') ? $errors->first('remarks') : '' }}</span>
-                        </div>
+                </div>
+                <div class="form-group {{ $errors->has('remarks') ? 'has-error' : '' }}">
+                    <label for="inputRemarks" class="col-sm-2 control-label">@lang('expense_template.field.remarks')</label>
+                    <div class="col-sm-8">
+                        <input id="inputRemarks" name="remarks" type="text" class="form-control" placeholder="@lang('expense_template.field.remarks')"
+                               data-parsley-required="true">
+                        <span class="help-block">{{ $errors->has('remarks') ? $errors->first('remarks') : '' }}</span>
                     </div>
-                    <div class="form-group">
-                        <label for="inputInternalExpense" class="col-sm-2 control-label">@lang('expense_template.field.internal_expense')</label>
-                        <div class="col-sm-8">
-                            <div class="checkbox icheck">
-                                <label>
-                                    <input type="checkbox" name="is_internal_expense" value="">&nbsp;
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputButton" class="col-sm-2 control-label"></label>
-                        <div class="col-sm-8">
-                            <a href="{{ route('db.master.expense_template') }}" class="btn btn-default">@lang('buttons.cancel_button')</a>
-                            <button class="btn btn-default" type="submit">@lang('buttons.create_new_button')</button>
+                </div>
+                <div class="form-group">
+                    <label for="inputInternalExpense" class="col-sm-2 control-label">@lang('expense_template.field.internal_expense')</label>
+                    <div class="col-sm-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox" name="is_internal_expense" value="">&nbsp;
+                            </label>
                         </div>
                     </div>
                 </div>
-                <div class="box-footer"></div>
+                <div class="form-group">
+                    <label for="inputButton" class="col-sm-2 control-label"></label>
+                    <div class="col-sm-8">
+                        <a href="{{ route('db.master.expense_template') }}" class="btn btn-default">@lang('buttons.cancel_button')</a>
+                        <button class="btn btn-default" type="submit">@lang('buttons.create_new_button')</button>
+                    </div>
+                </div>
             </div>
+            <div class="box-footer"></div>
         </form>
     </div>
 @endsection
 
 @section('custom_js')
     <script type="application/javascript">
-        $(function () {
+        $(document).ready(function () {
             $('input[type="checkbox"], input[type="radio"]').iCheck({
                 checkboxClass: 'icheckbox_square-blue',
                 radioClass: 'iradio_square-blue'
             });
         });
-
-        var app = angular.module('expenseTemplateModule', ['fcsa-number']);
-        app.controller("expenseTemplateController", ['$scope', function ($scope) {
-        }]);
     </script>
 @endsection
