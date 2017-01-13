52d09004378cea3e6af1be7cdbb03f216f715845
GitzJoey <gitzjoey@gmail.com>
2017-01-13 23:51:34 +0700

    test commit

--- resources/views/layouts/adminlte/sidebar.blade.php
+++ resources/views/layouts/adminlte/sidebar.blade.php
@@ -404,11 +404,7 @@
                         @if(Entrust::can('master.product-list') OR
                             Entrust::can('master.product-create') OR
                             Entrust::can('master.product-edit') OR
-                            Entrust::can('master.product-delete') OR
-                            Entrust::can('master.product.producttype-list') OR
-                            Entrust::can('master.product.producttype-create') OR
-                            Entrust::can('master.product.producttype-edit') OR
-                            Entrust::can('master.product.producttype-delete'))
+                            Entrust::can('master.product-delete'))
                             <li class="treeview {{ active_class(Active::checkRoutePattern('db.master.product') ||
                                                                 Active::checkRoutePattern('db.master.product.*') ||
                                                                 Active::checkRoutePattern('db.master.producttype') ||
