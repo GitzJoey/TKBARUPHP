96523875816022a19398d1eadafa6607791192af
GitzJoey <gitzjoey@gmail.com>
2017-01-29 03:50:45 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,9 +17,62 @@
 @endsection
 
 @section('content')
-    <div id="unfinishedSettingsNotice">
+    <div id="unfinishedSettingsNotice"></div>
 
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>150</h3>
+                    <p>&nbsp;&nbsp;&nbsp;</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-disc"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>53<sup style="font-size: 20px">%</sup></h3>
+                    <p>&nbsp;&nbsp;&nbsp;</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-stats"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-yellow">
+                <div class="inner">
+                    <h3>44</h3>
+                    <p>&nbsp;&nbsp;&nbsp;</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-person-add"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>65</h3>
+                    <p>&nbsp;&nbsp;&nbsp;</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-ios-pie"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
     </div>
+
     @for ($i = 0; $i < 100; $i++)
         <br/>
     @endfor
