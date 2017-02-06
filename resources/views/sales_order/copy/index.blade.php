47befbd08a10eb88527eaa16db2d21bee0a7491d
Decoydoll <miftah.9g@gmail.com>
2017-02-06 22:49:14 +0700

    Fix sweet alert on SO Copy index page

--- resources/views/sales_order/copy/index.blade.php
+++ resources/views/sales_order/copy/index.blade.php
@@ -88,7 +88,7 @@
                                    href="{{ route('db.so.copy.edit', ['soCode' => $copy->main_so_code, 'id' => $copy->hId()]) }}"
                                    title="Edit"><span class="fa fa-pencil fa-fw"></span></a>
                                 {!! Form::open(['method' => 'DELETE', 'route' => ['db.so.copy.delete', $copy->main_so_code, $copy->hId()], 'style'=>'display:inline'])  !!}
-                                <button type="submit" class="btn btn-xs btn-danger" title="Delete" id="delete_button">
+                                <button type="submit" class="btn btn-xs btn-danger" title="Delete" id="delete_button" v-on:click.prevent="showAlert">
                                     <span class="fa fa-close fa-fw"></span></button>
                                 {!! Form::close() !!}
                             </td>
@@ -106,27 +106,28 @@
 
 @section('custom_js')
     <script type="application/javascript">
-        $('#delete_button').on('click', function (e) {
-            e.preventDefault();
-            var form = $(this).parents('form');
-            swal({
-                title: "@lang('messages.alert.delete.sales_order.copy.title')",
-                text: "@lang('messages.alert.delete.sales_order.copy.text')",
-                type: "error",
-                showCancelButton: true,
-                confirmButtonClass: "btn-danger",
-                confirmButtonText: "@lang('buttons.reject_button')",
-                cancelButtonText: "@lang('buttons.cancel_button')",
-                closeOnConfirm: false
-            }, function (isConfirm) {
-                if (isConfirm) form.submit();
-            });
-        });
-
         var app = new Vue({
             el: '#soCopyVue',
             data: {
                 soCode: '{{ $soCode }}'
+            },
+            methods: {
+                showAlert: function (event) {
+                    var buttonId = event.currentTarget.id;
+                    var form = $('#'+buttonId).parents('form');
+                    swal({
+                        title: "@lang('messages.alert.delete.sales_order.copy.title')",
+                        text: "@lang('messages.alert.delete.sales_order.copy.text')",
+                        type: "error",
+                        showCancelButton: true,
+                        confirmButtonClass: "btn-danger",
+                        confirmButtonText: "@lang('buttons.reject_button')",
+                        cancelButtonText: "@lang('buttons.cancel_button')",
+                        closeOnConfirm: false
+                    }, function (isConfirm) {
+                        if (isConfirm) form.submit();
+                    });
+                }
             }
         });
     </script>
