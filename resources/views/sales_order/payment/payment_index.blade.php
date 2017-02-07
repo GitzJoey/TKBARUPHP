a89e162f8b8e4a2cd5b5745386de037bed8ef07d
gitzjoey <gitzjoey@live.com>
2017-02-07 08:27:04 +0700

    new info if no payments

--- resources/views/sales_order/payment/payment_index.blade.php
+++ resources/views/sales_order/payment/payment_index.blade.php
@@ -30,38 +30,44 @@
         <div class="box-body">
             <table class="table table-bordered">
                 <thead>
-                <tr>
-                    <th class="text-center">@lang('sales_order.payment.index.table.header.code')</th>
-                    <th class="text-center">@lang('sales_order.payment.index.table.header.customer')</th>
-                    <th class="text-center">@lang('sales_order.payment.index.table.header.so_date')</th>
-                    <th class="text-center">@lang('sales_order.payment.index.table.header.total')</th>
-                    <th class="text-center">@lang('sales_order.payment.index.table.header.paid')</th>
-                    <th class="text-center">@lang('sales_order.payment.index.table.header.rest')</th>
-                    <th class="text-center">@lang('labels.ACTION')</th>
-                </tr>
+                    <tr>
+                        <th class="text-center">@lang('sales_order.payment.index.table.header.code')</th>
+                        <th class="text-center">@lang('sales_order.payment.index.table.header.customer')</th>
+                        <th class="text-center">@lang('sales_order.payment.index.table.header.so_date')</th>
+                        <th class="text-center">@lang('sales_order.payment.index.table.header.total')</th>
+                        <th class="text-center">@lang('sales_order.payment.index.table.header.paid')</th>
+                        <th class="text-center">@lang('sales_order.payment.index.table.header.rest')</th>
+                        <th class="text-center">@lang('labels.ACTION')</th>
+                    </tr>
                 </thead>
                 <tbody>
-                @foreach ($salesOrders as $key => $so)
-                    <tr>
-                        <td class="text-center">{{ $so->code }}</td>
-                        <td class="text-center">
-                            @if($so->customer_type == 'CUSTOMERTYPE.R')
-                                {{ $so->customer->name }}
-                            @else
-                                {{ $so->walk_in_customer }}
-                            @endif
-                        </td>
-                        <td class="text-center">{{ date('d-m-Y', strtotime($so->so_created)) }}</td>
-                        <td class="text-center">{{ number_format($so->totalAmount(), 0) }}</td>
-                        <td class="text-center">{{ number_format($so->totalAmountPaid(), 0) }}</td>
-                        <td class="text-center">{{ number_format($so->totalAmount() - $so->totalAmountPaid(), 0) }}</td>
-                        <td class="text-center" width="10%">
-                            <a class="btn btn-xs btn-primary" href="{{ route('db.so.payment.cash', $so->hId()) }}" title="Cash"><span class="fa fa-money fa-fw"></span></a>
-                            <a class="btn btn-xs btn-primary" href="{{ route('db.so.payment.transfer', $so->hId()) }}" title="Transfer"><span class="fa fa-send fa-fw"></span></a>
-                            <a class="btn btn-xs btn-primary" href="{{ route('db.so.payment.giro', $so->hId()) }}" title="Giro"><span class="fa fa-book fa-fw"></span></a>
-                        </td>
-                    </tr>
-                @endforeach
+                    @if (count($salesOrders) > 0)
+                        @foreach ($salesOrders as $key => $so)
+                            <tr>
+                                <td class="text-center">{{ $so->code }}</td>
+                                <td class="text-center">
+                                    @if($so->customer_type == 'CUSTOMERTYPE.R')
+                                        {{ $so->customer->name }}
+                                    @else
+                                        {{ $so->walk_in_customer }}
+                                    @endif
+                                </td>
+                                <td class="text-center">{{ date('d-m-Y', strtotime($so->so_created)) }}</td>
+                                <td class="text-center">{{ number_format($so->totalAmount(), 0) }}</td>
+                                <td class="text-center">{{ number_format($so->totalAmountPaid(), 0) }}</td>
+                                <td class="text-center">{{ number_format($so->totalAmount() - $so->totalAmountPaid(), 0) }}</td>
+                                <td class="text-center" width="10%">
+                                    <a class="btn btn-xs btn-primary" href="{{ route('db.so.payment.cash', $so->hId()) }}" title="Cash"><span class="fa fa-money fa-fw"></span></a>
+                                    <a class="btn btn-xs btn-primary" href="{{ route('db.so.payment.transfer', $so->hId()) }}" title="Transfer"><span class="fa fa-send fa-fw"></span></a>
+                                    <a class="btn btn-xs btn-primary" href="{{ route('db.so.payment.giro', $so->hId()) }}" title="Giro"><span class="fa fa-book fa-fw"></span></a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td colspan="7" class="text-center animated shake">@lang('labels.DATA_NOT_FOUND')</td>
+                        </tr>
+                    @endif
                 </tbody>
             </table>
         </div>
