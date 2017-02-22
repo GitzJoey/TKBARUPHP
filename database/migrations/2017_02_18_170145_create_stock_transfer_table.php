8405afcaa42ff0436b59409e64b335538ccb41ab
jinrave <c.faishal.amrullah@gmail.com>
2017-02-22 15:17:00 +0700

    testing update

---
+++ database/migrations/2017_02_18_170145_create_stock_transfer_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockTransferTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_transfer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('store_id')->default(0);
+            $table->unsignedBigInteger('po_id')->default(0);
+            $table->unsignedBigInteger('product_id')->default(0);
+            $table->dateTime('transfer_date')->nullable();
+            $table->unsignedBigInteger('source_warehouse_id')->default(0);
+            $table->unsignedBigInteger('destination_warehouse_id')->default(0);
+            $table->decimal('quantity', 19, 2)->default(0);
+            $table->decimal('cost', 19, 2)->default(0);
+            $table->string('reason', 25)->nullable();
+            $table->unsignedBigInteger('created_by')->default(0);
+            $table->unsignedBigInteger('updated_by')->default(0);
+            $table->unsignedBigInteger('deleted_by')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_transfer');
+    }
+}
