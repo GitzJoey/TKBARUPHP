82a43a511a193be37c567cd9966d4dbe06718379
Masbro Web <daulayreza@gmail.com>
2017-03-09 09:34:29 +0700

    Add Stock History

---
+++ resources/lang/id/stock_history.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'page_title' => 'Histori Stok',
+    'header_title' => 'Daftar Tipe Produk',
+    'table' => [
+        'header' => [
+            'code' => 'PO Code',
+            'name' => 'Nama',
+            'short_code' => 'Kode',
+            'description' => 'Deskripsi',
+            'qty' => 'Qty',
+            'current_qty' => 'Qty Sekarang',
+            'date' => 'Tanggal',
+            'customer' => 'Pelanggan',
+            'so_code' => 'Kode SO',
+        ]
+    ],
+    'theres_no_data' => 'Tidak ada data',
+    'sales_history' => 'Histori Penjualan',
+    'stock_history' => 'Histori Stok',
+];
