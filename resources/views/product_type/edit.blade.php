f020a0ec89102eefeb591257c16515f0b363cadf
Sandi Sahdewo <sandi.sahdewo@gmail.com>
2017-06-04 23:07:50 +0700

    Change product type validate in view edit

--- resources/views/product_type/edit.blade.php
+++ resources/views/product_type/edit.blade.php
@@ -17,59 +17,115 @@
 @endsection
 
 @section('content')
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>@lang('labels.GENERAL_ERROR_TITLE')</strong> @lang('labels.GENERAL_ERROR_DESC')<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <div class="box box-info">
-        <div class="box-header with-border">
-            <h3 class="box-title">@lang('product_type.edit.header.title')</h3>
-        </div>
-        {!! Form::model($prodtype, ['method' => 'PATCH', 'route' => ['db.master.producttype.edit', $prodtype->hId()], 'class' => 'form-horizontal', 'data-parsley-validate' => 'true']) !!}
-            <div class="box-body">
-                <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
-                    <label for="inputName" class="col-sm-2 control-label">@lang('product_type.field.name')</label>
-                    <div class="col-sm-10">
-                        <input id="inputName" name="name" type="text" class="form-control" value="{{ $prodtype->name }}" placeholder="Name" data-parsley-required="true">
-                        <span class="help-block">{{ $errors->has('name') ? $errors->first('name') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('short_code') ? 'has-error' : '' }}">
-                    <label for="inputShortCode" class="col-sm-2 control-label">@lang('product_type.field.short_code')</label>
-                    <div class="col-sm-10">
-                        <input id="inputShortCode" name="short_code" type="text" class="form-control" value="{{ $prodtype->short_code }}" placeholder="Short Code">
-                        <span class="help-block">{{ $errors->has('short_code') ? $errors->first('short_code') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputDescription" class="col-sm-2 control-label">@lang('product_type.field.description')</label>
-                    <div class="col-sm-10">
-                        <input id="inputDescription" name="description" type="text" class="form-control" value="{{ $prodtype->description }}" placeholder="Description">
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('status') ? 'has-error' : '' }}">
-                    <label for="inputStatus" class="col-sm-2 control-label">@lang('product_type.field.status')</label>
-                    <div class="col-sm-10">
-                        {{ Form::select('status', $statusDDL, null, array('class' => 'form-control', 'placeholder' => Lang::get('labels.PLEASE_SELECT'), 'data-parsley-required' => 'true')) }}
-                        <span class="help-block">{{ $errors->has('status') ? $errors->first('status') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputButton" class="col-sm-2 control-label"></label>
-                    <div class="col-sm-10">
-                        <a href="{{ route('db.master.producttype') }}" class="btn btn-default">@lang('buttons.cancel_button')</a>
-                        <button class="btn btn-default" type="submit">@lang('buttons.submit_button')</button>
-                    </div>
-                </div>
+    <div id="prodTypeVue">
+        <div v-show="errors.count() > 0" v-cloak>
+            <div class="alert alert-danger">
+                <strong>@lang('labels.GENERAL_ERROR_TITLE')</strong> @lang('labels.GENERAL_ERROR_DESC')<br><br>
+                <ul v-for="(e, eIdx) in errors.all()">
+                    <li>@{{ e }}</li>
+                </ul>
             </div>
-            <div class="box-footer"></div>
-        {!! Form::close() !!}
+        </div>
+
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">@lang('product_type.edit.header.title')</h3>
+            </div>
+            <form id="prodTypeForm" class="form-horizontal" method="post" v-on:submit.prevent="validateBeforeSubmit()">
+                <div class="box-body">
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('name') }">
+                        <label for="inputName" class="col-sm-2 control-label">@lang('product_type.field.name')</label>
+                        <div class="col-sm-10">
+                            <input id="inputName" name="name" type="text" class="form-control" placeholder="@lang('product_type.field.name')"
+                                v-model="product_type.name" v-validate="'required'" data-vv-as="{{ trans('product_type.field.name') }}">
+                            <span v-show="errors.has('name')" class="help-block" v-cloak>@{{ errors.first('name') }}</span>
+                        </div>
+                    </div>
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('short_code') }">
+                        <label for="inputShortCode" class="col-sm-2 control-label">@lang('product_type.field.short_code')</label>
+                        <div class="col-sm-10">
+                            <input id="inputShortCode" name="short_code" type="text" class="form-control" placeholder="@lang('product_type.field.short_code')"
+                                v-model="product_type.short_code" v-validate="'required'" data-vv-as="{{ trans('product_type.field.short_code') }}">
+                            <span v-show="errors.has('short_code')" class="help-block" v-cloak>@{{ errors.first('short_code') }}</span>
+                        </div>
+                    </div>
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('description') }">
+                        <label for="inputDescription" class="col-sm-2 control-label">@lang('product_type.field.description')</label>
+                        <div class="col-sm-10">
+                            <input id="inputDescription" name="description" type="text" class="form-control" placeholder="@lang('product_type.field.description')"
+                                v-model="product_type.description" v-validate="'required'" data-vv-as="{{ trans('product_type.field.description') }}">
+                            <span v-show="errors.has('description')" class="help-block" v-cloak>@{{ errors.first('description') }}</span>
+                        </div>
+                    </div>
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('status') }">
+                        <label for="inputStatus" class="col-sm-2 control-label">@lang('product_type.field.status')</label>
+                        <div class="col-sm-10">
+                            <select class="form-control"
+                                    name="status"
+                                    v-model="product_type.status"
+                                    v-validate="'required'"
+                                    data-vv-as="{{ trans('product_type.field.status') }}">
+                                <option v-bind:value="defaultStatus">@lang('labels.PLEASE_SELECT')</option>
+                                <option v-for="(value, key) in statusDDL" v-bind:value="key">@{{ value }}</option>
+                            </select>
+                            <span v-show="errors.has('status')" class="help-block" v-cloak>@{{ errors.first('status') }}</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputButton" class="col-sm-2 control-label"></label>
+                        <div class="col-sm-10">
+                            <a href="{{ route('db.master.producttype') }}" class="btn btn-default">@lang('buttons.cancel_button')</a>
+                            <button class="btn btn-default" type="submit">@lang('buttons.submit_button')</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer"></div>
+            </form>
+        </div>
     </div>
+@endsection
+
+@section('custom_js')
+    <script type="application/javascript">
+        Vue.use(VeeValidate, { locale: '{!! LaravelLocalization::getCurrentLocale() !!}' });
+
+        var app = new Vue({
+            el: '#prodTypeVue',
+            data: {
+                product_type: {
+                    name:'{{ $prodtype->name }}',
+                    short_code:'{{ $prodtype->short_code }}',
+                    description:'{{ $prodtype->description }}',
+                    status: '{{ $prodtype->status }}',
+                },
+                statusDDL: JSON.parse('{!! htmlspecialchars_decode($statusDDL) !!}')
+            },
+            methods: {
+                validateBeforeSubmit: function() {
+                    var vm = this;
+                    this.$validator.validateAll().then(function(result) {
+                        $('#loader-container').fadeIn('fast');
+                        axios.post('{{ route('api.post.db.master.producttype.edit', $prodtype->hId()) }}' + '?api_token=' + $('#secapi').val(), new FormData($('#prodTypeForm')[0]))
+                            .then(function(response) {
+                                window.location.href = '{{ route('db.master.producttype') }}';
+                            }).catch(function(e) {
+                                $('#loader-container').fadeOut('fast');
+                                if (e.response.data.address.length > 0) {
+                                    for (var i=0; i < e.response.data.address.length; i++) {
+                                        vm.$validator.errorBag.add('', e.response.data.address[i], 'server', '__global__');
+                                    }
+                                } else {
+                                    vm.$validator.errorBag.add('', e.response.status + ' ' + e.response.statusText, 'server', '__global__');
+                                }
+                        });
+                    });
+                },
+            },
+            computed: {
+                defaultStatus: function() {
+                    return '';
+                }
+            }
+        });
+    </script>
 @endsection
