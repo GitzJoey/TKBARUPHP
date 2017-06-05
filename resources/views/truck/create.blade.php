250f90b360a6c0c9fb946245aa682876c2438c36
Sandi Sahdewo <sandi.sahdewo@gmail.com>
2017-06-05 23:23:56 +0700

    Delete method="post"

--- resources/views/truck/create.blade.php
+++ resources/views/truck/create.blade.php
@@ -30,7 +30,7 @@
             <div class="box-header with-border">
                 <h3 class="box-title">@lang('truck.create.header.title')</h3>
             </div>
-            <form id="truckForm" class="form-horizontal" method="post" v-on:submit.prevent="validateBeforeSubmit()">
+            <form id="truckForm" class="form-horizontal" v-on:submit.prevent="validateBeforeSubmit()">
                 {{ csrf_field() }}
                 <div class="box-body">
                     <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('truck_type') }">
