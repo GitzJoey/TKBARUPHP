bbf7439eac3556152eb8895c9b94c8505e517275
Sandi Sahdewo <sandi.sahdewo@gmail.com>
2017-06-05 22:01:00 +0700

    Change giro validate in view edit

--- resources/views/giro/edit.blade.php
+++ resources/views/giro/edit.blade.php
@@ -13,102 +13,169 @@
 @endsection
 
 @section('content')
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <strong>@lang('labels.GENERAL_ERROR_TITLE')</strong> @lang('labels.GENERAL_ERROR_DESC')<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <div class="box box-info">
-        <div class="box-header with-border">
-            <h3 class="box-title">@lang('giro.edit.header.title')</h3>
-        </div>
-        {!! Form::model($giro, ['method' => 'PATCH', 'route' => ['db.bank.giro.edit', $giro->hId()], 'class' => 'form-horizontal', 'data-parsley-validate' => 'parsley']) !!}
-            <div class="box-body">
-                <div class="form-group {{ $errors->has('plate_number') ? 'has-error' : '' }}">
-                    <label for="inputBank" class="col-sm-2 control-label">@lang('giro.field.bank')</label>
-                    <div class="col-sm-10">
-                        {{ Form::select('bank', $bankDDL, $giro->bank->id, array('class' => 'form-control', 'placeholder' => Lang::get('labels.PLEASE_SELECT'), 'data-parsley-required' => 'true')) }}
-                        <span class="help-block">{{ $errors->has('plate_number') ? $errors->first('plate_number') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('serial_number') ? 'has-error' : '' }}">
-                    <label for="inputSerialNumber" class="col-sm-2 control-label">@lang('giro.field.serial_number')</label>
-                    <div class="col-sm-10">
-                        <input id="inputSerialNumber" name="serial_number" type="text" value="{{ $giro->serial_number }}"class="form-control" placeholder="@lang('giro.field.serial_number')">
-                        <span class="help-block">{{ $errors->has('serial_number') ? $errors->first('serial_number') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('effective_date') ? 'has-error' : '' }}">
-                    <label for="inputEffectiveDate" class="col-sm-2 control-label">@lang('giro.field.effective_date')</label>
-                    <div class="col-sm-10">
-                        <div class="input-group date">
-                            <div class="input-group-addon">
-                                <i class="fa fa-calendar"></i>
-                            </div>
-                            <input type="text" class="form-control" id="inputEffectiveDate" name="effective_date" data-parsley-required="true" value="{{ date('d-m-Y', strtotime($giro->effective_date)) }}">
-                        </div>
-                        <span class="help-block">{{ $errors->has('effective_date') ? $errors->first('effective_date') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('amount') ? 'has-error' : '' }}">
-                    <label for="inputAmount" class="col-sm-2 control-label">@lang('giro.field.amount')</label>
-                    <div class="col-sm-10">
-                        <input id="inputAmount" name="amount" type="text" class="form-control" value="{{ $giro->amount }}" placeholder="@lang('giro.field.amount')"
-                               data-parsley-required="true"
-                               data-parsley-pattern="/^\d+(,\d+)*$/"
-                               autonumeric data-a-sep="," data-a-dec=".">
-                        <span class="help-block">{{ $errors->has('amount') ? $errors->first('amount') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('printed_name') ? 'has-error' : '' }}">
-                    <label for="inputPrintedName" class="col-sm-2 control-label">@lang('giro.field.printed_name')</label>
-                    <div class="col-sm-10">
-                        <input id="inputPrintedName" name="printed_name" type="text" class="form-control" value={{ $giro->printed_name }} placeholder="@lang('giro.field.printed_name')">
-                        <span class="help-block">{{ $errors->has('printed_name') ? $errors->first('printed_name') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('status') ? 'has-error' : '' }}">
-                    <label for="inputStatus" class="col-sm-2 control-label">@lang('giro.field.status')</label>
-                    <div class="col-sm-10">
-                        <input id="inputStatus" name="status" type="text" class="form-control" value="@lang('lookup.' . $giro->status)" readonly>
-                        <span class="help-block">{{ $errors->has('status') ? $errors->first('status') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group {{ $errors->has('remarks') ? 'has-error' : '' }}">
-                    <label for="inputRemarks" class="col-sm-2 control-label">@lang('giro.field.remarks')</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputRemarks" name="remarks" value="{{ $giro->remarks }}" placeholder="@lang('giro.field.remarks')">
-                        <span class="help-block">{{ $errors->has('remarks') ? $errors->first('remarks') : '' }}</span>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputButton" class="col-sm-2 control-label"></label>
-                    <div class="col-sm-10">
-                        <a href="{{ route('db.bank.giro') }}" class="btn btn-default">@lang('buttons.cancel_button')</a>
-                        <button class="btn btn-default" type="submit">@lang('buttons.submit_button')</button>
-                    </div>
-                </div>
+    <div id="giroVue">
+        <div v-show="errors.count() > 0" v-cloak>
+            <div class="alert alert-danger">
+                <strong>@lang('labels.GENERAL_ERROR_TITLE')</strong> @lang('labels.GENERAL_ERROR_DESC')<br><br>
+                <ul v-for="(e, eIdx) in errors.all()">
+                    <li>@{{ e }}</li>
+                </ul>
             </div>
-            <div class="box-footer"></div>
-        {!! Form::close() !!}
+        </div>
+
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h3 class="box-title">@lang('giro.edit.header.title')</h3>
+            </div>
+            <form id="giroForm" class="form-horizontal" method="post" v-on:submit.prevent="validateBeforeSubmit()">
+                <div class="box-body">
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('bank') }">
+                        <label for="inputBank" class="col-sm-2 control-label">@lang('giro.field.bank')</label>
+                        <div class="col-sm-10">
+                            <select class="form-control"
+                                    name="bank"
+                                    v-model="giro.bank"
+                                    v-validate="'required'"
+                                    data-vv-as="{{ trans('giro.field.bank') }}">
+                                <option v-bind:value="defaultBank">@lang('labels.PLEASE_SELECT')</option>
+                                <option v-for="(value, key) in bankDDL" v-bind:value="key">@{{ value }}</option>
+                            </select>
+                            <span v-show="errors.has('bank')" class="help-block" v-cloak>@{{ errors.first('bank') }}</span>
+                        </div>
+                    </div>
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('serial_number') }">
+                        <label for="inputSerialNumber" class="col-sm-2 control-label">@lang('giro.field.serial_number')</label>
+                        <div class="col-sm-10">
+                            <input id="inputSerialNumber" name="serial_number" type="text" class="form-control" placeholder="@lang('giro.field.serial_number')"
+                                v-model="giro.serial_number" v-validate="'required'" data-vv-as="{{ trans('giro.field.serial_number') }}">
+                            <span v-show="errors.has('serial_number')" class="help-block" v-cloak>@{{ errors.first('serial_number') }}</span>
+                        </div>
+                    </div>
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('effective_date') }">
+                        <label for="inputEffectiveDate" class="col-sm-2 control-label">@lang('giro.field.effective_date')</label>
+                        <div class="col-sm-9">
+                            <div class="input-group date">
+                                <div class="input-group-addon">
+                                    <i class="fa fa-calendar"></i>
+                                </div>
+                                <vue-datetimepicker name="effective_date" value="" v-model="giro.effective_date" v-validate="'required'" format="DD-MM-YYYY hh:mm A"></vue-datetimepicker>
+                                <span v-show="errors.has('effective_date')" class="help-block" v-cloak>@{{ errors.first('effective_date') }}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group {{ $errors->has('amount') ? 'has-error' : '' }}">
+                        <label for="inputAmount" class="col-sm-2 control-label">@lang('giro.field.amount')</label>
+                        <div class="col-sm-10">
+                            <input id="inputAmount" name="amount" type="text" class="form-control" value="{{ $giro->amount }}" placeholder="@lang('giro.field.amount')"
+                                   data-parsley-required="true"
+                                   data-parsley-pattern="/^\d+(,\d+)*$/"
+                                   autonumeric data-a-sep="," data-a-dec=".">
+                            <span class="help-block">{{ $errors->has('amount') ? $errors->first('amount') : '' }}</span>
+                        </div>
+                    </div>
+                    <div v-bind:class="{ 'form-group':true, 'has-error':errors.has('printed_name') }">
+                        <label for="inputPrintedName" class="col-sm-2 control-label">@lang('giro.field.printed_name')</label>
+                        <div class="col-sm-10">
+                            <input id="inputPrintedName" name="printed_name" type="text" class="form-control" placeholder="@lang('giro.field.printed_name')"
+                                v-model="giro.printed_name" v-validate="'required'" data-vv-as="{{ trans('giro.field.printed_name') }}">
+                            <span v-show="errors.has('printed_name')" class="help-block" v-cloak>@{{ errors.first('printed_name') }}</span>
+                        </div>
+                    </div>
+                    <div class="form-group {{ $errors->has('status') ? 'has-error' : '' }}">
+                        <label for="inputStatus" class="col-sm-2 control-label">@lang('giro.field.status')</label>
+                        <div class="col-sm-10">
+                            <input id="inputStatus" name="status" type="text" class="form-control" value="@lang('lookup.' . $giro->status)" readonly>
+                            <span class="help-block">{{ $errors->has('status') ? $errors->first('status') : '' }}</span>
+                        </div>
+                    </div>
+                    <div class="form-group {{ $errors->has('remarks') ? 'has-error' : '' }}">
+                        <label for="inputRemarks" class="col-sm-2 control-label">@lang('giro.field.remarks')</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputRemarks" name="remarks" value="{{ $giro->remarks }}" placeholder="@lang('giro.field.remarks')">
+                            <span class="help-block">{{ $errors->has('remarks') ? $errors->first('remarks') : '' }}</span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputButton" class="col-sm-2 control-label"></label>
+                        <div class="col-sm-10">
+                            <a href="{{ route('db.bank.giro') }}" class="btn btn-default">@lang('buttons.cancel_button')</a>
+                            <button class="btn btn-default" type="submit">@lang('buttons.submit_button')</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer"></div>
+            </form>
+        </div>
     </div>
 @endsection
 
 @section('custom_js')
     <script type="application/javascript">
-        $(document).ready(function() {
-            $('#inputEffectiveDate').datetimepicker({
-                format: 'DD-MM-YYYY',
-                defaultDate: moment().toDate(),
-                showTodayButton: true,
-                showClose: true
-            });
+        Vue.use(VeeValidate, { locale: '{!! LaravelLocalization::getCurrentLocale() !!}' });
+
+        Vue.component('vue-datetimepicker', {
+            template: "<input type='text' v-bind:id='id' v-bind:name='name' class='form-control' v-bind:value='value' v-model='value' v-bind:format='format' v-bind:readonly='readonly'>",
+            props: ['id', 'name', 'value', 'format', 'readonly'],
+            mounted: function() {
+                var vm = this;
+
+                if (this.value == undefined || this.value == NaN) this.value = '';
+                if (this.format == undefined || this.format == NaN) this.format = 'DD-MM-YYYY hh:mm A';
+                if (this.readonly == undefined || this.readonly == NaN) this.readonly = 'false';
+
+                $(this.$el).datetimepicker({
+                    format: this.format,
+                    defaultDate: this.value == '' ? moment():moment(this.value),
+                    showTodayButton: true,
+                    showClose: true
+                }).on("dp.change", function(e) {
+                    vm.$emit('input', this.value);
+                });
+
+                if (this.value == '') { vm.$emit('input', moment().format(this.format)); }
+            },
+            destroyed: function() {
+                $(this.$el).data("DateTimePicker").destroy();
+            }
+        });
+
+        var app = new Vue({
+            el: '#giroVue',
+            data: {
+                giro: {
+                    serial_number:'{{ $giro->serial_number }}',
+                    printed_name:'{{ $giro->printed_name }}',
+                    bank:'{{ $giro->bank }}',
+                    effective_date:'{{ $giro->effective_date }}'
+                },
+                bankDDL: JSON.parse('{!! htmlspecialchars_decode($bankDDL) !!}')
+            },
+            methods: {
+                validateBeforeSubmit: function() {
+                    var vm = this;
+                    this.$validator.validateAll().then(function(result) {
+                        $('#loader-container').fadeIn('fast');
+                        axios.post('{{ route('api.post.db.bank.giro.edit', $giro->hId()) }}' + '?api_token=' + $('#secapi').val(), new FormData($('#giroForm')[0]))
+                            .then(function(response) {
+                                window.location.href = '{{ route('db.bank.giro') }}';
+                            }).catch(function(e) {
+                                $('#loader-container').fadeOut('fast');
+                                if (e.response.data.address.length > 0) {
+                                    for (var i=0; i < e.response.data.address.length; i++) {
+                                        vm.$validator.errorBag.add('', e.response.data.address[i], 'server', '__global__');
+                                    }
+                                } else {
+                                    vm.$validator.errorBag.add('', e.response.status + ' ' + e.response.statusText, 'server', '__global__');
+                                }
+                        });
+                    });
+                },
+            },
+            computed: {
+                defaultBank: function() {
+                    return '';
+                }
+            }
         });
     </script>
 @endsection
