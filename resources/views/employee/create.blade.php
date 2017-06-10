ff7e0aeb57d320863ac0f5010130e2a7a4d122d3
Sandi Sahdewo <sandi.sahdewo@gmail.com>
2017-06-10 10:45:20 +0700

    remove comments

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -182,7 +182,7 @@
                         $('#loader-container').fadeIn('fast');
                         axios.post('{{ route('api.post.db.employee.create') }}' + '?api_token=' + $('#secapi').val(), new FormData($('#employeeForm')[0]))
                             .then(function(response) {
-                                /*window.location.href = '{{ route('db.employee.employee') }}';*/
+                                window.location.href = '{{ route('db.employee.employee') }}';
                             }).catch(function(e) {
                                 $('#loader-container').fadeOut('fast');
                                 if (e.response.data.address.length > 0) {
