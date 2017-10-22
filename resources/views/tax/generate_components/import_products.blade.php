e2c9a514fcf59e92f588b535bb7212e64a27da20
Farid Inawan <frdteknikelektro@gmail.com>
2017-10-23 00:51:58 +0700

    Remove KODE_OBJEK data, and fix alignment

--- resources/views/tax/generate_components/import_products.blade.php
+++ resources/views/tax/generate_components/import_products.blade.php
@@ -2,18 +2,18 @@
   <table class="table table-bordered">
     <thead>
         <tr>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.ob')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.object_code')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.name')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.unit_price')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.ob')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.object_code')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.name')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_products.table.header.unit_price')</th>
         </tr>
     </thead>
     <tbody>
         <tr v-for="product in products" v-cloak>
-            <td class="text-center">OB</td>
-            <td class="text-center">@{{ product.id }}</td>
-            <td class="text-center">@{{ product.name }}</td>
-            <td class="text-center">@{{ numeral(product.price).format() }}</td>
+            <td class="text-left">OB</td>
+            <td class="text-left">-</td>
+            <td class="text-left">@{{ product.name }}</td>
+            <td class="text-right">@{{ numeral(product.price).format() }}</td>
         </tr>
         <tr v-if="!products.length">
             <td class="text-center" colspan="8">@lang('labels.DATA_NOT_FOUND')</td>
