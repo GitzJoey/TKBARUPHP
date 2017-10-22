689a3bea8f0d32d7e9b9de15cd6c2e3ce22553c1
Farid Inawan <frdteknikelektro@gmail.com>
2017-10-23 00:56:42 +0700

    Change almost all alignment to left.

--- resources/views/tax/generate_components/import_opponents.blade.php
+++ resources/views/tax/generate_components/import_opponents.blade.php
@@ -2,38 +2,38 @@
   <table class="table table-bordered">
     <thead>
         <tr>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.lt')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.tax_id')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.name')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.street')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.block')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.number')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.rt')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.rw')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.district')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.village')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.region')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.province')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.postal_code')</th>
-            <th class="text-center" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.phone_number')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.lt')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.tax_id')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.name')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.street')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.block')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.number')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.rt')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.rw')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.district')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.village')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.region')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.province')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.postal_code')</th>
+            <th class="text-left" style="vertical-align:middle">@lang('tax.generate.import_opponents.table.header.phone_number')</th>
         </tr>
     </thead>
     <tbody>
         <tr v-for="taxOutput in taxesOutput" v-cloak>
-            <td class="text-center">LT</td>
-            <td class="text-center">@{{ taxOutput.opponentTaxIdNo }}</td>
-            <td class="text-center">@{{ taxOutput.opponentName }}</td>
-            <td class="text-center">@{{ taxOutput.opponentAddress }}</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
-            <td class="text-center">-</td>
+            <td class="text-left">LT</td>
+            <td class="text-left">@{{ taxOutput.opponentTaxIdNo }}</td>
+            <td class="text-left">@{{ taxOutput.opponentName }}</td>
+            <td class="text-left">@{{ taxOutput.opponentAddress }}</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
+            <td class="text-left">-</td>
         </tr>
         <tr v-if="!products.length">
             <td class="text-center" colspan="14">@lang('labels.DATA_NOT_FOUND')</td>
