99047b6595532d7a47ee52338f7e54eb347c6a07
GitzJoey <gitzjoey@gmail.com>
2017-11-28 22:58:25 +0700

    prepare for stock merge module

---
+++ database/migrations/2017_11_28_155654_create_stock_merger_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockMergerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_merger', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('merge_type')->nullable();
+            $table->dateTime('merge_date')->nullable();
+            $table->string('remarks')->nullable();
+            $table->unsignedBigInteger('created_by')->default(0);
+            $table->unsignedBigInteger('updated_by')->default(0);
+            $table->unsignedBigInteger('deleted_by')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_merger');
+    }
+}
---
+++ database/migrations/2017_11_28_155712_create_stock_merge_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockMergeDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_merge_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('stock_merger_id')->default(0);
+            $table->unsignedBigInteger('po_id')->default(0);
+            $table->decimal('before_merge_qty', 19, 2)->default(0);
+            $table->unsignedBigInteger('created_by')->default(0);
+            $table->unsignedBigInteger('updated_by')->default(0);
+            $table->unsignedBigInteger('deleted_by')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_merge_details');
+    }
+}
