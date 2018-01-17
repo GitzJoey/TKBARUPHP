5f102327b34e66852d48f3e21b48b163ed8a8098
GitzJoey <gitzjoey@live.com>
2018-01-17 11:42:11 +0700

    fix store create

--- resources/views/store/create.blade.php
+++ resources/views/store/create.blade.php
@@ -469,8 +469,20 @@
                             , new FormData($('#storeForm')[0])
                             , { headers: { 'content-type': 'multipart/form-data' } })
                             .then(function(response) {
-                                if (response.data.result == 'success') { window.location.href = '{{ route('db.admin.store') }}'; }
-                            });
+                                window.location.href = '{{ route('db.admin.store') }}';
+                            }).catch(function (e) {
+                                $('#loader-container').fadeOut('fast');
+                                if (e.response.data.errors != undefined && Object.keys(e.response.data.errors).length > 0) {
+                                    for (var key in e.response.data.errors) {
+                                        for (var i = 0; i < e.response.data.errors[key].length; i++) {
+                                            vm.$validator.errors.add('', e.response.data.errors[key][i], 'server', '__global__');
+                                        }
+                                    }
+                                } else {
+                                    vm.$validator.errors.add('', e.response.status + ' ' + e.response.statusText, 'server', '__global__');
+                                    if (e.response.data.message != undefined) { console.log(e.response.data.message); }
+                                }
+                        });
                     });
                 },
                 addNewBank: function() {
